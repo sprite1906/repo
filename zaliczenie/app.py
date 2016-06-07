6aed3f4c4a9531affd3060446fcb868b65038880
EC2 Default User <ec2-user@ip-172-31-31-73.eu-central-1.compute.internal>
2016-06-07 17:06:50 +0000

    reszta

--- zaliczenie/app.py
+++ zaliczenie/app.py
@@ -1,7 +1,10 @@
 import os, boto3
+from album_queue import request_album
 from uuid import uuid4
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, send_from_directory
 app = Flask(__name__)
+
+bucket_address = 'https://s3.eu-central-1.amazonaws.com/167429-sprite'
 
 @app.route("/hello")
 def hello():
@@ -14,20 +17,36 @@ def index():
 @app.route("/upload", methods=['POST'])
 def upload():
   files = request.files
+  album = {
+    'photos': []
+  }
   for f in files.getlist('file'):
     print f
-    upload_s3(f)
-    filename = f.filename
-    updir = '/home/ec2-user/photoAlbumUi/upload'
-    f.save(os.path.join(updir, filename))
+    destination_name = generate_filename(f)
+    album['photos'].append('%s/%s' % (bucket_address, destination_name))
+    upload_s3(f, destination_name)
+  return jsonify(album)
+
+@app.route("/request-album", methods=['POST'])
+def request_album_creation():
+  album = {
+    'photos': [
+      'https://s3.eu-central-1.amazonaws.com/153412-kkanclerz/photos/009d30b3d9a143a5937fbab9a50a4009/empty_image.jpg',
+      'https://s3.eu-central-1.amazonaws.com/153412-kkanclerz/photos/009d30b3d9a143a5937fbab9a50a4009/empty_image.jpg'
+    ]
+  }
+  request_album(album)
   return jsonify()
 
-def upload_s3(source_file):
-  bucket_name = '153412-kkanclerz'
-  destination_filename = "photos/%s/%s" % (uuid4().hex, source_file.filename)
+def upload_s3(source_file, destination_filename):
+  bucket_name = '167429-sprite'
   s3 = boto3.resource('s3')
   bucket = s3.Bucket(bucket_name)
   bucket.put_object(Key=destination_filename, Body=source_file)
+
+def generate_filename(source_file):
+  destination_filename = "photos/%s/%s" % (uuid4().hex, source_file.filename)
+  return destination_filename
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
